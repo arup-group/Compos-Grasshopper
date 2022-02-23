5b5d2c884bbf731c35717da0a8e3133932ba5899
Kristjan Nielsen <kristjan.nielsen@arup.com>
2022-02-23 18:54:15 +0100

    parameters, properties and UI

---
+++ ComposGH/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using ComposGH;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(ComposGHInfo.ProductName)]
+[assembly: AssemblyDescription(ComposGHInfo.Company + " " + ComposGHInfo.ProductName + " Grasshopper plugin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(ComposGHInfo.Company)]
+[assembly: AssemblyProduct(ComposGHInfo.ProductName)]
+[assembly: AssemblyCopyright(ComposGHInfo.Copyright)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5274ea0f-5cf8-4f74-ace6-a273f4196522")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(ComposGHInfo.Vers)]
+[assembly: AssemblyFileVersion(ComposGHInfo.Vers + ".0")]
---
+++ ComposGH/UI/AboutBox.cs
@@ -0,0 +1,110 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ComposGH.UI
+{
+    partial class AboutComposBox : Form
+    {
+        public AboutComposBox()
+        {
+            GH_AssemblyInfo composPlugin = Grasshopper.Instances.ComponentServer.FindAssembly(new Guid("c3884cdc-ac5b-4151-afc2-93590cef4f8f"));
+
+            string api = "Compos 8.6 COM API"; // IVersion.Api();
+            string pluginvers = composPlugin.Version;
+            string pluginloc = composPlugin.Location;
+
+            InitializeComponent();
+            this.Text = String.Format("About {0}", ComposGHInfo.PluginName);
+            this.labelProductName.Text = ComposGHInfo.ProductName + " Grasshopper plugin";
+            this.labelVersion.Text = String.Format("Version {0}", pluginvers);
+            this.labelApiVersion.Text = String.Format("API Version {0}", api);
+            this.labelCompanyName.Text = AssemblyCompany;
+            this.linkWebsite.Text = @"www.oasys-software.com";
+            this.labelContact.Text = "Contact and support:"; 
+            this.linkEmail.Text = @"oasys@arup.com";
+        }
+
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                return "Compos is a unique composite beam analysis and design software program. In addition to composite beam design and analysis, " +
+                    "Compos can also perform footfall induced vibration analysis for regular composite floors using Resotec Damping System.";
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                return ComposGHInfo.Copyright;
+            }
+        }
+        #endregion
+
+        private void labelProductName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelVersion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutBox_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(@"https://www.oasys-software.com/");
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"rhino://package/search?name=compos");
+        }
+
+        private void linkEmail_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            GH_AssemblyInfo gsaPlugin = Grasshopper.Instances.ComponentServer.FindAssembly(new Guid("a3b08c32-f7de-4b00-b415-f8b466f05e9f"));
+            string pluginvers = gsaPlugin.Version;
+            Process.Start(@"mailto:oasys@arup.com?subject=Oasys " + ComposGHInfo.PluginName + " version " + pluginvers);
+        }
+
+        private void labelApiVersion_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
