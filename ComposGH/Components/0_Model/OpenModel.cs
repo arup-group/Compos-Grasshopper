c5308d277a5fe96182c5ee890aa830adc1309504
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-03-30 16:13:07 +0200

    manual merge from dev-gianluca

---
+++ ComposGH/Components/0_Model/OpenModel.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using Grasshopper.Kernel.Attributes;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.GUI;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Windows.Forms;
+using Grasshopper.Kernel.Types;
+using ComposGH.Parameters;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace ComposGH.Components
+{
+    public class OpenModel : GH_Component
+    {
+        #region Name and Ribbon Layout
+        // This region handles how the component in displayed on the ribbon
+        // including name, exposure level and icon
+        public override Guid ComponentGuid => new Guid("555839bf-08ae-45bd-87c2-84c65d3dc115");
+        public OpenModel()
+          : base("Open Model", "Open", "Open a Compos Model",
+                Ribbon.CategoryName.Name(),
+                Ribbon.SubCategoryName.Cat0())
+        { this.Hidden = true; } // sets the initial state of the component to hidden
+
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.OpenModel;
+
+        #endregion
+
+        #region Custom UI
+        //This region overrides the typical component layout
+        public override void CreateAttributes()
+        {
+            m_attributes = new UI.ButtonComponentUI(this, "Open", OpenFile, "Open Compos file");
+        }
+        public void OpenFile()
+        {
+            var fdi = new Rhino.UI.OpenFileDialog { Filter = "Compos Files(*.cob)|*.coa|All files (*.*)|*.*" };
+            var res = fdi.ShowOpenDialog();
+            if (res)
+            {
+                fileName = fdi.FileName;
+
+                // instantiate  new panel
+                var panel = new Grasshopper.Kernel.Special.GH_Panel();
+                panel.CreateAttributes();
+
+                // set the location relative to the open component on the canvas
+                panel.Attributes.Pivot = new PointF((float)Attributes.DocObject.Attributes.Bounds.Left -
+                    panel.Attributes.Bounds.Width - 30, (float)Params.Input[0].Attributes.Pivot.Y - panel.Attributes.Bounds.Height / 2);
+
+                // check for existing input
+                while (Params.Input[0].Sources.Count > 0)
+                {
+                    var input = Params.Input[0].Sources[0];
+                    // check if input is the one we automatically create below
+                    if (Params.Input[0].Sources[0].InstanceGuid == panelGUID)
+                    {
+                        // update the UserText in existing panel
+                        //RecordUndoEvent("Changed OpenCompos Component input");
+                        panel = input as Grasshopper.Kernel.Special.GH_Panel;
+                        panel.UserText = fileName;
+                        panel.ExpireSolution(true); // update the display of the panel
+                    }
+
+                    // remove input
+                    Params.Input[0].RemoveSource(input);
+                }
+
+                //populate panel with our own content
+                panel.UserText = fileName;
+
+                // record the panel's GUID if new, so that we can update it on change
+                panelGUID = panel.InstanceGuid;
+
+                //Until now, the panel is a hypothetical object.
+                // This command makes it 'real' and adds it to the canvas.
+                Grasshopper.Instances.ActiveCanvas.Document.AddObject(panel, false);
+
+                //Connect the new slider to this component
+                Params.Input[0].AddSource(panel);
+
+                (this as IGH_VariableParameterComponent).VariableParameterMaintenance();
+                Params.OnParametersChanged();
+
+                ExpireSolution(true);
+            }
+        }
+        #endregion
+
+        #region Input and output
+        // This region handles input and output parameters
+
+        string fileName = null;
+        Guid panelGUID = Guid.NewGuid();
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Filename and path", "File", "Compos model to open and work with." +
+                    System.Environment.NewLine + "Input either path component, a text string with path and " +
+                    System.Environment.NewLine + "filename or an existing Compos model created in Grasshopper.", GH_ParamAccess.item);
+        }
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Compos Model", "Compos", "Compos Model", GH_ParamAccess.item);
+        }
+
+        #endregion
+
+        #region (de)serialization
+        //This region handles serialisation and deserialisation, meaning that 
+        // component states will be remembered when reopening GH script
+        public override bool Write(GH_IO.Serialization.GH_IWriter writer)
+        {
+            //writer.SetInt32("Mode", (int)_mode);
+            writer.SetString("File", (string)fileName);
+            //writer.SetBoolean("Advanced", (bool)advanced);
+            return base.Write(writer);
+        }
+        public override bool Read(GH_IO.Serialization.GH_IReader reader)
+        {
+            //_mode = (FoldMode)reader.GetInt32("Mode");
+            fileName = (string)reader.GetString("File");
+            //advanced = (bool)reader.GetBoolean("Advanced");
+            return base.Read(reader);
+        }
+        #endregion
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            GH_ObjectWrapper gh_typ = new GH_ObjectWrapper();
+
+            DA.GetData(0, ref gh_typ);
+
+            if (gh_typ.Value is GH_String)
+            {
+                string tempfile = "";
+                if (GH_Convert.ToString(gh_typ, out tempfile, GH_Conversion.Both))
+                    fileName = tempfile;
+
+                if (!fileName.EndsWith(".cob"))
+                    fileName += ".cob";
+
+
+                ComposModel composModel = new ComposModel
+                {
+                    FileName = fileName
+                };
+
+                string path = System.IO.Path.GetTempPath() + "Compos.coa";
+
+                composModel.Model.Open(fileName);
+                composModel.Model.SaveAs(path);
+
+                DA.SetData(0, new ComposModelGoo(composModel));
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ ComposGH/Parameters/ComposSteelMaterial.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Rhino.Geometry;
+using Rhino;
+using Grasshopper.Documentation;
+using Rhino.Collections;
+
+namespace ComposGH.Parameters
+{
+    /// <summary>
+    /// Section class, this class defines the basic properties and methods for any Compos Section
+    /// </summary>
+    public class ComposSteelMaterial
+    {
+
+        public enum SteelType
+        {
+            CUSTOM = 0,
+            S235 = 1,
+            S275 = 2,
+            S355 = 3,
+            S450 = 4,
+            S460 = 5
+        }
+
+
+        public string WeldMaterial
+        {
+            get 
+            { return wMat; } 
+            set
+            { wMat = value; }
+        }
+
+        public double YeldStrength
+        {
+            get
+            { return fy; }
+            set
+            { fy = value; }
+        }
+
+        public double YoungModulus
+        {
+            get
+            { return E; }
+            set
+            { E = value; }
+        }
+
+        public double Density
+        {
+            get
+            { return ρ; }
+            set
+            { ρ = value; }
+        }
+
+
+        public SteelType SType;
+
+        
+        #region fields
+        string wMat = "";
+        double fy;
+        double E;
+        double ρ;
+
+        #endregion
+
+        #region constructors
+        public ComposSteelMaterial()
+        {
+            SType = SteelType.CUSTOM;
+        }
+
+        /// <param name="material_id"></param>
+        public ComposSteelMaterial(int material_id)
+        {
+            SType = (SteelType)material_id;
+        }
+
+  
+
+        #endregion
+
+        
+        #region properties
+        public bool IsValid
+        {
+            get
+            {
+                return true;
+            }
+        }
+        #endregion
+
+        #region methods
+        public override string ToString()
+        {
+            string mate = SType.ToString();
+            mate = Char.ToUpper(mate[0]) + mate.Substring(1).ToLower().Replace("_", " ");
+
+            return "Compos Steel Material " + mate;
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// GsaSection Goo wrapper class, makes sure GsaSection can be used in Grasshopper.
+    /// </summary>
+    public class ComposSteelMaterialGoo : GH_Goo<ComposSteelMaterial>
+    {
+        #region constructors
+        public ComposSteelMaterialGoo()
+        {
+            this.Value = new ComposSteelMaterial();
+        }
+        public ComposSteelMaterialGoo(ComposSteelMaterial material)
+        {
+            if (material == null)
+                material = new ComposSteelMaterial();
+            this.Value = material; //material.Duplicate();
+        }
+
+        public override IGH_Goo Duplicate()
+        {
+            return DuplicateGsaSection();
+        }
+        public ComposSteelMaterialGoo DuplicateGsaSection()
+        {
+            return new ComposSteelMaterialGoo(Value == null ? new ComposSteelMaterial() : Value); //Value.Duplicate());
+        }
+        #endregion
+
+        #region properties
+        public override bool IsValid
+        {
+            get
+            {
+                if (Value == null) { return false; }
+                return true;
+            }
+        }
+        public override string IsValidWhyNot
+        {
+            get
+            {
+                //if (Value == null) { return "No internal GsaMember instance"; }
+                if (Value.IsValid) { return string.Empty; }
+                return Value.IsValid.ToString(); //Todo: beef this up to be more informative.
+            }
+        }
+        public override string ToString()
+        {
+            if (Value == null)
+                return "Null Compos Steel Material";
+            else
+                return Value.ToString();
+        }
+        public override string TypeName
+        {
+            get { return ("Compos Steel Material"); }
+        }
+        public override string TypeDescription
+        {
+            get { return ("Compos Steel Material"); }
+        }
+
+
+        #endregion
+
+        #region casting methods
+        public override bool CastTo<Q>(ref Q target)
+        {
+            // This function is called when Grasshopper needs to convert this 
+            // instance of GsaMaterial into some other type Q.            
+
+
+            if (typeof(Q).IsAssignableFrom(typeof(ComposSteelMaterial)))
+            {
+                if (Value == null)
+                    target = default;
+                else
+                    target = (Q)(object)Value;
+                return true;
+            }
+
+            target = default;
+            return false;
+        }
+        public override bool CastFrom(object source)
+        {
+            // This function is called when Grasshopper needs to convert other data 
+            // into ComposMaterial.
+
+            if (source == null) { return false; }
+
+            //Cast from GsaMaterial
+            if (typeof(ComposSteelMaterial).IsAssignableFrom(source.GetType()))
+            {
+                Value = (ComposSteelMaterial)source;
+                return true;
+            }
+
+            //Cast from string
+            if (GH_Convert.ToString(source, out string mat, GH_Conversion.Both))
+            {
+                if (mat.ToUpper() == "CUSTOM")
+                {
+                    Value.SType = ComposSteelMaterial.SteelType.CUSTOM;
+                    return true;
+                }
+
+                if (mat.ToUpper() == "S235")
+                {
+                    Value.SType = ComposSteelMaterial.SteelType.S235;
+                    return true;
+                }
+
+                if (mat.ToUpper() == "S275")
+                {
+                    Value.SType = ComposSteelMaterial.SteelType.S275;
+                    return true;
+                }
+
+                if (mat.ToUpper() == "S355")
+                {
+                    Value.SType = ComposSteelMaterial.SteelType.S355;
+                    return true;
+                }
+
+                if (mat.ToUpper() == "S450")
+                {
+                    Value.SType = ComposSteelMaterial.SteelType.S450;
+                    return true;
+                }
+
+                if (mat.ToUpper() == "S460")
+                {
+                    Value.SType = ComposSteelMaterial.SteelType.S460;
+                    return true;
+                }
+
+                return false;
+            }
+
+            return false;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// This class provides a Parameter interface for the Data_GsaSection type.
+    /// </summary>
+    public class ComposSteelMaterialParameter : GH_PersistentParam<ComposSteelMaterialGoo>
+    {
+        public ComposSteelMaterialParameter()
+          : base(new GH_InstanceDescription("Material", "Ma", "Compos Material", ComposGH.Components.Ribbon.CategoryName.Name(), ComposGH.Components.Ribbon.SubCategoryName.Cat9()))
+        {
+        }
+
+        public override Guid ComponentGuid => new Guid("f13d079b-f7d1-4d8a-be7c-3b7e1e59c5ab");
+
+        public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.obscure;
+
+        protected override System.Drawing.Bitmap Icon => ComposGH.Properties.Resources.SteelMaterialParam;
+
+        protected override GH_GetterResult Prompt_Plural(ref List<ComposSteelMaterialGoo> values)
+        {
+            return GH_GetterResult.cancel;
+        }
+        protected override GH_GetterResult Prompt_Singular(ref ComposSteelMaterialGoo value)
+        {
+            return GH_GetterResult.cancel;
+        }
+        protected override System.Windows.Forms.ToolStripMenuItem Menu_CustomSingleValueItem()
+        {
+            System.Windows.Forms.ToolStripMenuItem item = new System.Windows.Forms.ToolStripMenuItem
+            {
+                Text = "Not available",
+                Visible = false
+            };
+            return item;
+        }
+        protected override System.Windows.Forms.ToolStripMenuItem Menu_CustomMultiValueItem()
+        {
+            System.Windows.Forms.ToolStripMenuItem item = new System.Windows.Forms.ToolStripMenuItem
+            {
+                Text = "Not available",
+                Visible = false
+            };
+            return item;
+        }
+
+        #region preview methods
+
+        public bool Hidden
+        {
+            get { return true; }
+            //set { m_hidden = value; }
+        }
+        public bool IsPreviewCapable
+        {
+            get { return false; }
+        }
+        #endregion
+    }
+
+}
