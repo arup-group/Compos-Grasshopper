2d53d4ac25f7119a5fba0346b477eee6b91b6e37
Ilona Laskowska <Ilona.Laskowska@arup.com>
2022-04-06 23:10:02 +0200

    res

--- ComposGH/Properties/Resources.Designer.cs
+++ ComposGH/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace ComposGH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap RebarMesh {
+            get {
+                object obj = ResourceManager.GetObject("RebarMesh", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap SaveModel {
             get {
                 object obj = ResourceManager.GetObject("SaveModel", resourceCulture);
