171214acd48a7901907902a94cf875137de44005
kpne <kpn@kpne.dk>
2022-04-21 10:22:35 +0200

    added more tests

--- ComposGH/Tests/Parameters/ComposDesignCodeTest.cs
+++ ComposGH/Tests/Parameters/ComposDesignCodeTest.cs
@@ -26,14 +26,25 @@ namespace ComposGH.Parameters.Tests
       Code code = Code.EN1994_1_1_2004;
       NationalAnnex nationalAnnex = NationalAnnex.Generic;
 
-      ComposDesignCode designCode = new ComposDesignCode(code, nationalAnnex);
+      // create with constructor and duplicate
+      ComposDesignCode original = new ComposDesignCode(code, nationalAnnex);
+      ComposDesignCode duplicate = original.Duplicate();
+      
+      // check that duplicate has duplicated values
+      Assert.Equal(code, duplicate.Design_Code);
+      Assert.Equal(nationalAnnex, duplicate.National_Annex);
 
-      ComposDesignCode duplicate = designCode.Duplicate();
+      // make some changes to duplicate
       duplicate.Design_Code = Code.BS5950_3_1_1990_Superseeded;
       duplicate.National_Annex = NationalAnnex.United_Kingdom;
 
-      Assert.Equal(code, designCode.Design_Code);
-      Assert.Equal(nationalAnnex, designCode.National_Annex);
+      // check that duplicate has set changes
+      Assert.Equal(Code.BS5950_3_1_1990_Superseeded, duplicate.Design_Code);
+      Assert.Equal(NationalAnnex.United_Kingdom, duplicate.National_Annex);
+
+      // check that original keeps original values
+      Assert.Equal(code, original.Design_Code);
+      Assert.Equal(nationalAnnex, original.National_Annex);
     }
   }
 }
