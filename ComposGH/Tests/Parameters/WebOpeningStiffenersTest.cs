8e41c40593a36de779dbb7c1c11a44c51423afd5
kpne <kpn@kpne.dk>
2022-04-21 12:04:09 +0200

    stud spec test and updated comments

--- ComposGH/Tests/Parameters/WebOpeningStiffenersTest.cs
+++ ComposGH/Tests/Parameters/WebOpeningStiffenersTest.cs
@@ -6,6 +6,7 @@ namespace ComposGH.Parameters.Tests
 {
   public partial class ComposWebOpeningTest
   {
+    // 1 setup inputs
     [Theory]
     [InlineData(50, 100, 10, 100, 10, false)]
     [InlineData(20, 120, 5, 80, 12, true)]
@@ -14,11 +15,13 @@ namespace ComposGH.Parameters.Tests
     {
       LengthUnit unit = LengthUnit.Millimeter;
 
+      // 2 create object instance with constructor
       WebOpeningStiffeners webOpeningStiffeners = new WebOpeningStiffeners(
         new Length(startPos, unit), new Length(topWidth, unit), 
         new Length(topTHK, unit), new Length(bottomWidth, unit), 
         new Length(bottomTHK, unit), bothSides);
 
+      // 3 check that inputs are set in object's members
       Assert.Equal(startPos, webOpeningStiffeners.DistanceFrom.Millimeters);
       Assert.Equal(topWidth, webOpeningStiffeners.TopStiffenerWidth.Millimeters);
       Assert.Equal(topTHK, webOpeningStiffeners.TopStiffenerThickness.Millimeters);
@@ -27,8 +30,11 @@ namespace ComposGH.Parameters.Tests
       Assert.Equal(bothSides, webOpeningStiffeners.isBothSides);
       Assert.False(webOpeningStiffeners.isNotch);
 
+      // (optionally return object for other tests)
       return webOpeningStiffeners;
     }
+
+    // 1 setup inputs
     [Theory]
     [InlineData(50, 100, 10, false)]
     [InlineData(20, 120, 5, true)]
@@ -37,10 +43,12 @@ namespace ComposGH.Parameters.Tests
     {
       LengthUnit unit = LengthUnit.Millimeter;
 
+      // 2 create object instance with constructor
       WebOpeningStiffeners webOpeningStiffeners = new WebOpeningStiffeners(
         new Length(distance, unit), new Length(topWidth, unit),
         new Length(topTHK, unit), bothSides);
 
+      // 3 check that inputs are set in object's members
       Assert.Equal(distance, webOpeningStiffeners.DistanceFrom.Millimeters);
       Assert.Equal(topWidth, webOpeningStiffeners.TopStiffenerWidth.Millimeters);
       Assert.Equal(topTHK, webOpeningStiffeners.TopStiffenerThickness.Millimeters);
@@ -49,16 +57,20 @@ namespace ComposGH.Parameters.Tests
       Assert.Equal(bothSides, webOpeningStiffeners.isBothSides);
       Assert.True(webOpeningStiffeners.isNotch);
 
+      // (optionally return object for other tests)
       return webOpeningStiffeners;
     }
+
     [Fact]
     public void TestStiffenerDuplicate()
     {
       LengthUnit unit = LengthUnit.Millimeter;
 
+      // 1 create with constructor and duplicate
       WebOpeningStiffeners original = TestConstructorStiffenersWebOpening(25, 75, 12, 125, 15, false);
       WebOpeningStiffeners duplicate = original.Duplicate();
 
+      // 2 check that duplicate has duplicated values
       Assert.Equal(25, duplicate.DistanceFrom.Millimeters);
       Assert.Equal(75, duplicate.TopStiffenerWidth.Millimeters);
       Assert.Equal(12, duplicate.TopStiffenerThickness.Millimeters);
@@ -67,6 +79,7 @@ namespace ComposGH.Parameters.Tests
       Assert.False(duplicate.isBothSides);
       Assert.False(duplicate.isNotch);
 
+      // 3 make some changes to duplicate
       duplicate.DistanceFrom = new Length(26, unit);
       duplicate.TopStiffenerWidth = new Length(76, unit);
       duplicate.TopStiffenerThickness = new Length(13, unit);
@@ -74,14 +87,7 @@ namespace ComposGH.Parameters.Tests
       duplicate.BottomStiffenerThickness = new Length(16, unit);
       duplicate.isBothSides = true;
 
-      Assert.Equal(25, original.DistanceFrom.Millimeters);
-      Assert.Equal(75, original.TopStiffenerWidth.Millimeters);
-      Assert.Equal(12, original.TopStiffenerThickness.Millimeters);
-      Assert.Equal(125, original.BottomStiffenerWidth.Millimeters);
-      Assert.Equal(15, original.BottomStiffenerThickness.Millimeters);
-      Assert.False(original.isBothSides);
-      Assert.False(original.isNotch);
-
+      // 4 check that duplicate has set changes
       Assert.Equal(26, duplicate.DistanceFrom.Millimeters);
       Assert.Equal(76, duplicate.TopStiffenerWidth.Millimeters);
       Assert.Equal(13, duplicate.TopStiffenerThickness.Millimeters);
@@ -90,21 +96,35 @@ namespace ComposGH.Parameters.Tests
       Assert.True(duplicate.isBothSides);
       Assert.False(duplicate.isNotch);
 
+      // 5 check that original has not been changed
+      Assert.Equal(25, original.DistanceFrom.Millimeters);
+      Assert.Equal(75, original.TopStiffenerWidth.Millimeters);
+      Assert.Equal(12, original.TopStiffenerThickness.Millimeters);
+      Assert.Equal(125, original.BottomStiffenerWidth.Millimeters);
+      Assert.Equal(15, original.BottomStiffenerThickness.Millimeters);
+      Assert.False(original.isBothSides);
+      Assert.False(original.isNotch);
+
+      // 1 create with new constructor and duplicate
       original = TestConstructorStiffenersNotch(27, 77, 14, true);
       duplicate = original.Duplicate();
+
+      // 2 check that duplicate has duplicated values
+      Assert.Equal(27, duplicate.DistanceFrom.Millimeters);
+      Assert.Equal(77, duplicate.TopStiffenerWidth.Millimeters);
+      Assert.Equal(14, duplicate.TopStiffenerThickness.Millimeters);
+      Assert.Equal(Length.Zero, duplicate.BottomStiffenerWidth);
+      Assert.Equal(Length.Zero, duplicate.BottomStiffenerThickness);
+      Assert.True(duplicate.isBothSides);
+      Assert.True(duplicate.isNotch);
+
+      // 3 make some changes to duplicate
       duplicate.DistanceFrom = new Length(28, unit);
       duplicate.TopStiffenerWidth = new Length(78, unit);
       duplicate.TopStiffenerThickness = new Length(15, unit);
       duplicate.isBothSides = false;
 
-      Assert.Equal(27, original.DistanceFrom.Millimeters);
-      Assert.Equal(77, original.TopStiffenerWidth.Millimeters);
-      Assert.Equal(14, original.TopStiffenerThickness.Millimeters);
-      Assert.Equal(Length.Zero, original.BottomStiffenerWidth);
-      Assert.Equal(Length.Zero, original.BottomStiffenerThickness);
-      Assert.True(original.isBothSides);
-      Assert.True(original.isNotch);
-
+      // 4 check that duplicate has set changes
       Assert.Equal(28, duplicate.DistanceFrom.Millimeters);
       Assert.Equal(78, duplicate.TopStiffenerWidth.Millimeters);
       Assert.Equal(15, duplicate.TopStiffenerThickness.Millimeters);
@@ -112,6 +132,15 @@ namespace ComposGH.Parameters.Tests
       Assert.Equal(Length.Zero, duplicate.BottomStiffenerThickness);
       Assert.False(duplicate.isBothSides);
       Assert.True(duplicate.isNotch);
+
+      // 5 check that original has not been changed
+      Assert.Equal(27, original.DistanceFrom.Millimeters);
+      Assert.Equal(77, original.TopStiffenerWidth.Millimeters);
+      Assert.Equal(14, original.TopStiffenerThickness.Millimeters);
+      Assert.Equal(Length.Zero, original.BottomStiffenerWidth);
+      Assert.Equal(Length.Zero, original.BottomStiffenerThickness);
+      Assert.True(original.isBothSides);
+      Assert.True(original.isNotch);
     }
   }
 }
