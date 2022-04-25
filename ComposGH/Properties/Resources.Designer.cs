cb1c6630d78eb8a97d6f17bfc1e412b106d676b3
kpne <kpn@kpne.dk>
2022-04-25 20:19:38 +0200

    create concrete material icon

--- ComposGH/Properties/Resources.Designer.cs
+++ ComposGH/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace ComposGH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap CreateConcreteMaterial {
+            get {
+                object obj = ResourceManager.GetObject("CreateConcreteMaterial", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap CreateDesignCode {
             get {
                 object obj = ResourceManager.GetObject("CreateDesignCode", resourceCulture);
