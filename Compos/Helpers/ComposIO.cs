84cf3d0da9cff9086754510a74282bac0bdf8498
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-05-03 11:47:53 +0200

    open

--- Compos/Helpers/ComposIO.cs
+++ Compos/Helpers/ComposIO.cs
@@ -4,11 +4,22 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Compos_8_6;
 
 namespace ComposAPI
 {
   public static class ComposIO
   {
     public static string InstallPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Oasys", "Compos 8.6");
+
+    public static IAutomation Open(string pathName)
+    {
+      IAutomation automation = new Automation();
+      automation.Open(pathName);
+      return automation;
+    }
+
+
+
   }
 }
