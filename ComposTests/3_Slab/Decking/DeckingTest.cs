38550e4ac6561ace0555d67d55ac6c8debaec657
kpne <kpn@kpne.dk>
2022-07-15 09:48:41 +0200

    removed sqlite reference from test project

--- ComposTests/3_Slab/Decking/DeckingTest.cs
+++ ComposTests/3_Slab/Decking/DeckingTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
 using UnitsNet;
 using UnitsNet.Units;
 using Xunit;
