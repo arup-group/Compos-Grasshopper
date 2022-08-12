c09abd7b266bbdadd5683942b2e2c0fb74b8c35d
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-08-12 14:07:58 +0200

    test .coa files

--- ComposTests/0_File/ComposFileTest.cs
+++ ComposTests/0_File/ComposFileTest.cs
@@ -16,20 +16,6 @@ namespace ComposAPI.File.Tests
     [Theory]
     [InlineData("*.coa")]
     [InlineData("*.cob")]
-    public void OpenTest(string searchPattern)
-    {
-      string path = Path.GetFullPath(ComposFileTest.RelativePath);
-
-      foreach (string fileName in Directory.GetFiles(path, searchPattern, SearchOption.TopDirectoryOnly))
-      {
-        ComposFile file = ComposFile.Open(fileName);
-        Assert.NotNull(file);
-      }
-    }
-
-    [Theory]
-    [InlineData("*.coa")]
-    [InlineData("*.cob")]
     public void AnalyseTest(string searchPattern)
     {
       string path = Path.GetFullPath(ComposFileTest.RelativePath);
@@ -41,5 +27,58 @@ namespace ComposAPI.File.Tests
         Assert.Equal(0, status);
       }
     }
+
+    [Theory]
+    [InlineData("Compos1.coa", "MEMBER-1")]
+    public void AnalyseMemberTest(string fileName, string memberName)
+    {
+      ComposFile file = ComposFile.Open(Path.GetFullPath(ComposFileTest.RelativePath + fileName));
+      short status = file.Analyse(memberName);
+      Assert.Equal(0, status);
+    }
+
+    [Theory]
+    [InlineData("Compos1.coa", "MEMBER-1", 3)]
+    public void CodeSatisfiedTest(string fileName, string memberName, int expextedStatus)
+    {
+      ComposFile file = ComposFile.Open(Path.GetFullPath(ComposFileTest.RelativePath + fileName));
+      short status = file.CodeSatisfied(memberName);
+      Assert.Equal(expextedStatus, status);
+    }
+
+
+    [Theory]
+    [InlineData("*.coa")]
+    [InlineData("*.cob")]
+    public void DesignTest(string searchPattern)
+    {
+      string path = Path.GetFullPath(ComposFileTest.RelativePath);
+
+      foreach (string fileName in Directory.GetFiles(path, searchPattern, SearchOption.TopDirectoryOnly))
+      {
+        ComposFile file = ComposFile.Open(fileName);
+        short status = file.Design();
+        Assert.Equal(0, status);
+      }
+    }
+
+    [Theory]
+    [InlineData("Compos1.coa", "MEMBER-1")]
+    public void DesignMemberTest(string fileName, string memberName)
+    {
+      ComposFile file = ComposFile.Open(Path.GetFullPath(ComposFileTest.RelativePath + fileName));
+      short status = file.Design(memberName);
+      Assert.Equal(0, status);
+    }
+
+    [Theory]
+    [InlineData("Compos1.coa", "MEMBER-1")]
+    public void BeamSectDescTest(string fileName, string memberName)
+    {
+      ComposFile file = ComposFile.Open(Path.GetFullPath(ComposFileTest.RelativePath + fileName));
+      string status = file.BeamSectDesc(memberName);
+      Assert.Equal("STD I 600. 200. 15. 25.", status);
+    }
+
   }
 }
