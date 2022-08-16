addbf02e43597a88629c0d88606dda3437b57f50
kpne <kpn@kpne.dk>
2022-08-16 17:11:04 +0200

    CreateStandardASNZSteelMaterialComponentTests

---
+++ ComposGHTests/Components/1_Beam/3_Tertiary/CreateStandardASNZSteelMaterialComponentTests.cs
@@ -0,0 +1,49 @@
+﻿using ComposAPI;
+using ComposGH.Parameters;
+using ComposGH.Components;
+using Xunit;
+using ComposGHTests.Helpers;
+
+namespace ComposGHTests
+{
+  [Collection("GrasshopperFixture collection")]
+  public class CreateStandardASNZSteelMaterialComponentTests
+  {
+    [Fact]
+    public void CreateComponentTest()
+    {
+      var comp = new CreateStandardASNZSteelMaterial();
+      comp.CreateAttributes();
+
+      SteelMaterialGoo output = (SteelMaterialGoo)ComponentTestHelper.GetOutput(comp);
+      Assert.Equal(StandardASNZSteelMaterialGrade.C450_AS1163, ((ASNZSteelMaterial)output.Value).Grade);
+    }
+
+    [Fact]
+    public void CreateComponentWithInputsTest2()
+    {
+      var comp = new CreateStandardASNZSteelMaterial();
+      comp.CreateAttributes();
+
+      string input1 = "Gr250L15_AS3678";
+      ComponentTestHelper.SetInput(comp, input1, 0);
+
+      SteelMaterialGoo output = (SteelMaterialGoo)ComponentTestHelper.GetOutput(comp);
+      Assert.Equal(StandardASNZSteelMaterialGrade.Gr250L15_AS3678, ((ASNZSteelMaterial)output.Value).Grade);
+    }
+
+    [Fact]
+    public void DeserializeTest()
+    {
+      var comp = new CreateStandardASNZSteelMaterial();
+      OasysDropDownComponentTestHelper.TestDeserialize(comp);
+    }
+
+    [Fact]
+    public void ChangeDropDownTest()
+    {
+      var comp = new CreateStandardASNZSteelMaterial();
+      OasysDropDownComponentTestHelper.ChangeDropDownTest(comp);
+    }
+  }
+}
