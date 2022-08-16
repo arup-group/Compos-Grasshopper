02b1e41b7cc61126adbca6c721b05c9a25870af2
kpne <kpn@kpne.dk>
2022-08-16 16:28:53 +0200

    CreateStandardSteelMaterialComponentTests

--- ComposGHTests/Components/1_Beam/3_Tertiary/CreateStandardSteelMaterialComponentTests.cs
+++ ComposGHTests/Components/1_Beam/3_Tertiary/CreateStandardSteelMaterialComponentTests.cs
@@ -12,42 +12,37 @@ namespace ComposGHTests
     [Fact]
     public void CreateComponentTest()
     {
-      var comp = CreateStandardSteelMaterial();
+      var comp = new CreateStandardSteelMaterial();
+      comp.CreateAttributes();
 
       SteelMaterialGoo output = (SteelMaterialGoo)ComponentTestHelper.GetOutput(comp);
-      Assert.Equal(500, output.Value.fy.Value);
-      Assert.Equal(205000, output.Value.E.Value);
-      Assert.Equal(7850, output.Value.Density.Value);
-      Assert.False(output.Value.ReductionFactorMpl);
-      Assert.Equal(WeldMaterialGrade.Grade_35, output.Value.WeldGrade);
+      Assert.Equal(StandardSteelGrade.S235, output.Value.Grade);
     }
 
     [Fact]
     public void CreateComponentWithInputsTest2()
     {
-      GH_OasysDropDownComponent comp = CreateCustomSteelMaterialComponentMother();
+      var comp = new CreateStandardSteelMaterial();
+      comp.CreateAttributes();
 
-      bool input4 = true;
-      string input5 = "Grade_50";
-      ComponentTestHelper.SetInput(comp, input4, 3);
-      ComponentTestHelper.SetInput(comp, input5, 4);
+      string input1 = "S275";
+      ComponentTestHelper.SetInput(comp, input1, 0);
       
       SteelMaterialGoo output = (SteelMaterialGoo)ComponentTestHelper.GetOutput(comp);
-      Assert.Equal(input4, output.Value.ReductionFactorMpl);
-      Assert.Equal(WeldMaterialGrade.Grade_50, output.Value.WeldGrade);
+      Assert.Equal(StandardSteelGrade.S275, output.Value.Grade);
     }
 
     [Fact]
     public void DeserializeTest()
     {
-      GH_OasysDropDownComponent comp = CreateCustomSteelMaterialComponentMother();
+      var comp = new CreateStandardSteelMaterial();
       OasysDropDownComponentTestHelper.TestDeserialize(comp);
     }
 
     [Fact]
     public void ChangeDropDownTest()
     {
-      GH_OasysDropDownComponent comp = CreateCustomSteelMaterialComponentMother();
+      var comp = new CreateStandardSteelMaterial();
       OasysDropDownComponentTestHelper.ChangeDropDownTest(comp);
     }
   }
