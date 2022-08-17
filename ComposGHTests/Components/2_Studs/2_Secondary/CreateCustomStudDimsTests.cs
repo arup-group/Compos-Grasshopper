b2d21db7b4b32098ec241afb5e02e930875defdb
kpne <kpn@kpne.dk>
2022-08-17 13:31:40 +0200

    CreateCustomStudDimsTests

---
+++ ComposGHTests/Components/2_Studs/2_Secondary/CreateCustomStudDimsTests.cs
@@ -0,0 +1,53 @@
+﻿using ComposAPI;
+using ComposGH.Parameters;
+using ComposGH.Components;
+using Xunit;
+using ComposGHTests.Helpers;
+using UnitsNet;
+using UnitsNet.Units;
+
+namespace ComposGHTests
+{
+  [Collection("GrasshopperFixture collection")]
+  public class CreateCustomStudDimsTests
+  {
+    public static GH_OasysDropDownComponent CreateCustomStudDimsMother()
+    {
+      var comp = new CreateCustomStudDimensions();
+      comp.CreateAttributes();
+
+      int i = 0;
+      ComponentTestHelper.SetInput(comp, 21, i++);
+      ComponentTestHelper.SetInput(comp, 110, i++);
+      ComponentTestHelper.SetInput(comp, 500, i++);
+
+      return comp;
+    }
+
+    [Fact]
+    public void CreateComponentTest()
+    {
+      var comp = CreateCustomStudDimsMother();
+      comp.SetSelected(0, 1); // change the dropdown to cm
+      comp.SetSelected(1, 0); // change the dropdown to N
+      StudDimensionsGoo output = (StudDimensionsGoo)ComponentTestHelper.GetOutput(comp);
+      Assert.Equal(21, output.Value.Diameter.Centimeters);
+      Assert.Equal(110, output.Value.Height.Centimeters);
+      Assert.Equal(500, output.Value.CharacterStrength.Newtons);
+    }
+
+    [Fact]
+    public void DeserializeTest()
+    {
+      var comp = CreateCustomStudDimsMother();
+      OasysDropDownComponentTestHelper.TestDeserialize(comp);
+    }
+
+    [Fact]
+    public void ChangeDropDownTest()
+    {
+      var comp = CreateCustomStudDimsMother();
+      OasysDropDownComponentTestHelper.ChangeDropDownTest(comp);
+    }
+  }
+}
