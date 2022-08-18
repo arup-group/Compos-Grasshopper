c89cd4d6843d16d26d5c14a3703710eda60dc6eb
kpne <kpn@kpne.dk>
2022-08-18 18:06:34 +0200

    finished slab tests

---
+++ ComposGHTests/Components/3_Slab/1_Primary/CreateSlabTests.cs
@@ -0,0 +1,64 @@
+﻿using ComposAPI;
+using ComposGH.Parameters;
+using ComposGH.Components;
+using Xunit;
+using ComposGHTests.Helpers;
+using UnitsNet.Units;
+using static ComposAPI.ConcreteMaterial;
+
+namespace ComposGHTests.Slab
+{
+  [Collection("GrasshopperFixture collection")]
+  public class CreateSlabTests
+  {
+    public static GH_OasysComponent CreateSlabMother()
+    {
+      var comp = new CreateSlab();
+      comp.CreateAttributes();
+
+      ConcreteMaterialGoo input1 = (ConcreteMaterialGoo)ComponentTestHelper.GetOutput(CreateConcreteMaterialENTests.CreateConcreteMaterialENMother());
+      ComponentTestHelper.SetInput(comp, input1, 0);
+
+      SlabDimensionGoo input2 = (SlabDimensionGoo)ComponentTestHelper.GetOutput(CreateSlabDimensionTests.CreateSlabDimensionMother());
+      ComponentTestHelper.SetInput(comp, input2, 1);
+
+      TransverseReinforcementGoo input3 = (TransverseReinforcementGoo)ComponentTestHelper.GetOutput(CreateTransverseReinforcementTests.CreateTransverseReinforcementMother());
+      ComponentTestHelper.SetInput(comp, input3, 2);
+
+      return comp;
+    }
+
+    [Fact]
+    public void CreateComponentWithInputs1()
+    {
+      var comp = CreateSlabMother();
+      SlabGoo output = (SlabGoo)ComponentTestHelper.GetOutput(comp);
+
+      ConcreteMaterialGoo input1 = (ConcreteMaterialGoo)ComponentTestHelper.GetOutput(CreateConcreteMaterialENTests.CreateConcreteMaterialENMother());
+      Duplicates.AreEqual(input1.Value, output.Value.Material);
+
+      SlabDimensionGoo input2 = (SlabDimensionGoo)ComponentTestHelper.GetOutput(CreateSlabDimensionTests.CreateSlabDimensionMother());
+      ComponentTestHelper.SetInput(comp, input2, 1);
+      Duplicates.AreEqual(input2.Value, output.Value.Dimensions[0]);
+
+      TransverseReinforcementGoo input3 = (TransverseReinforcementGoo)ComponentTestHelper.GetOutput(CreateTransverseReinforcementTests.CreateTransverseReinforcementMother());
+      Duplicates.AreEqual(input3.Value, output.Value.Transverse);
+    }
+
+    [Fact]
+    public void CreateComponentWithInputs2()
+    {
+      var comp = CreateSlabMother();
+
+      MeshReinforcementGoo input4 = (MeshReinforcementGoo)ComponentTestHelper.GetOutput(CreateMeshReinforcementTests.CreateMeshReinforcementMother());
+      ComponentTestHelper.SetInput(comp, input4, 3);
+
+      DeckingGoo input5 = (DeckingGoo)ComponentTestHelper.GetOutput(CreateCatalogueDeckTests.CreateCatalogueDeckMother());
+      ComponentTestHelper.SetInput(comp, input5, 4);
+
+      SlabGoo output = (SlabGoo)ComponentTestHelper.GetOutput(comp);
+      Duplicates.AreEqual(input4.Value, output.Value.Mesh);
+      Duplicates.AreEqual(input5.Value, output.Value.Decking);
+    }
+  }
+}
