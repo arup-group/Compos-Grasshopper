4c4282da52fbc09de38fd2a363af6e8fe699b914
kpne <kpn@kpne.dk>
2022-09-05 09:20:42 +0200

    missing icon

--- ComposGH/Properties/Resources.Designer.cs
+++ ComposGH/Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace ComposGH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ComposInfo {
+            get {
+                object obj = ResourceManager.GetObject("ComposInfo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ComposLogo128 {
             get {
                 object obj = ResourceManager.GetObject("ComposLogo128", resourceCulture);
