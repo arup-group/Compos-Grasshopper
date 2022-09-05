8ab1252e0659fb4a5cf4f594fe3ed89293ad4ba0
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-09-05 12:11:12 +0200

    this

--- ComposGH/Components/GH_OasysDropDownComponent.cs
+++ ComposGH/Components/GH_OasysDropDownComponent.cs
@@ -18,6 +18,8 @@ namespace ComposGH.Components
     internal List<string> SelectedItems;
     internal List<string> SpacerDescriptions;
     internal bool IsInitialised = false;
+    internal bool UpdateOutput = true;
+    internal Dictionary<int, List<int>> ExistingOutputsSerialized = new Dictionary<int, List<int>>() { { 0, new List<int>() { 0 } } }; // new dictionary with key = 0 (inputid) and list of serialized ints with initial one item = 0
 
     #region UI
     public override void CreateAttributes()
@@ -30,11 +32,9 @@ namespace ComposGH.Components
 
     protected override void ExpireDownStreamObjects()
     {
-      if (UpdateOutput)
+      if (this.UpdateOutput)
         base.ExpireDownStreamObjects();
     }
-    internal bool UpdateOutput = true;
-    internal Dictionary<int, List<int>> ExistingOutputsSerialized = new Dictionary<int, List<int>>() { { 0, new List<int>() { 0 } } }; // new dictionary with key = 0 (inputid) and list of serialized ints with initial one item = 0
 
     internal abstract void InitialiseDropdowns();
 
