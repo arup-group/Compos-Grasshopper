0dc7d4ae5f4dca84cdcb7c98e7a77bfd0d270e76
Kristjan Nielsen <Kristjan.Nielsen@arup.com>
2022-09-06 12:49:42 +0200

    clean up IComposFile interface https://github.com/arup-group/Compos-Grasshopper/pull/85#discussion_r962735940

--- Compos/0_File/IComposFile.cs
+++ Compos/0_File/IComposFile.cs
@@ -7,25 +7,13 @@ namespace ComposAPI
   {
     Guid Guid { get; }
     void AddMember(IMember member);
-    //short Analyse();
     short Analyse(string memberName);
     short CodeSatisfied(string memberName);
     IList<IMember> GetMembers();
-    //short Design();
     short Design(string memberName);
     string BeamSectDesc(string memberName);
     IMember GetMember(string name);
-    //float MaxResult(string memberName, string option, short position);
-    //short MaxResultPosition(string memberName, string option, short position);
     string MemberName(int index);
-    //float MinResult(string memberName, string option, short position);
-    //short MinResultPosition(string memberName, string option, short position);
-    //short NumIntermediatePos(string memberName);
-    //short NumTranRebar(string memberName);
-    //float Result(string memberName, string option, short position);
     int SaveAs(string fileName);
-    //float TranRebarProp(string memberName, TransverseRebarOption option, short rebarnum);
-    //float UtilisationFactor(string memberName, UtilisationFactorOption option);
-    void Update();
   }
 }
