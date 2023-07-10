e5024e740e1f6f767b94028f25f467de84155a7a
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-07-10 15:36:11 +0200

    GSAGH-333 OasysGH version

--- ComposGH/ComposGHInfo.cs
+++ ComposGH/ComposGHInfo.cs
@@ -127,7 +127,7 @@ namespace ComposGH {
     public const string Copyright = "Copyright © Oasys 1985 - 2022";
     public const string PluginName = "ComposGH";
     public const string ProductName = "Compos";
-    public const string Vers = "0.9.12";
+    public const string Vers = "0.9.13";
     public static string Disclaimer = PluginName + " is pre-release and under active development, including further testing to be undertaken. It is provided \"as-is\" and you bear the risk of using it. Future versions may contain breaking changes. Any files, results, or other types of output information created using " + PluginName + " should not be relied upon without thorough and independent checking.";
     public static Guid GUID = new Guid("c3884cdc-ac5b-4151-afc2-93590cef4f8f");
     public static bool isBeta = true;
